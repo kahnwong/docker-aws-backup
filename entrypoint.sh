ec210d8eb101d6ecf3e0940d2bc6d8e08d5e1cfb
Karn Wong <karn@karnwong.me>
2024-03-02 01:20:43 +0700

    add archive mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-echo "foo"
+# set filename
+current_date=$(date +%Y-%m-%d)
+backup_prefix="s3://backup/$current_date"
+filename="$SERVICE_NAME-$current_date.tar.gz"
+
+# backup
+if [ "$MODE" = "ARCHIVE" ]; then
+	tar -czf "$filename" "$BACKUP_PATH"
+else
+	echo "$MODE is not  supported"
+fi
+
+# upload
+aws s3 cp --endpoint-url "$S3_ENDPOINT" "$filename" "$backup_prefix/$filename"
+curl -d "Successfully backup $SERVICE_NAME" https://ntfy.karnwong.me/nuc-backup
